d7a47d9e44f67dcac3b5b20449ffbe68758233e4
nghiahsgs <nghiahsgs@gmail.com>
2019-06-27 15:58:25 +0700

    make it better

---
+++ index.php
@@ -0,0 +1,186 @@
+<!-- http://preview.themeforest.net/item/magicbook-3d-responsive-flip-book-html-theme/full_screen_preview/7273418 -->
+<!DOCTYPE html>
+<!-- saved from url=(0056)http://www.kevin-li.com/magicbook/index-illustrator.html -->
+<html lang="en" class="js backgroundsize cssanimations csstransforms csstransforms3d csstransitions fontface csstransformspreserve3d" style=""><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"> 
+    <meta name="description" content="MagicBook - 3D Responsive FlipBook Creative HTML Template">
+    <meta name="keywords" content="MagicBook, BookBlock, book preview, html5, css3, 3d, perspective, fullscreen">
+    <meta name="author" content="KevinStudio">
+    
+    <title>MagicBook - 3D Responsive FlipBook Creative HTML Template</title>
+    
+    <!-- FAV and TOUCH ICONS -->
+    <link rel="shortcut icon" href="http://www.kevin-li.com/magicbook/img/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://www.kevin-li.com/magicbook/img/ico/logo-144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://www.kevin-li.com/magicbook/img/ico/logo-114.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://www.kevin-li.com/magicbook/img/ico/logo-72.png">
+    <link rel="apple-touch-icon-precomposed" href="http://www.kevin-li.com/magicbook/img/ico/logo-57.png">
+
+    <script async="" src="./index_files/analytics.js.download"></script><script src="./index_files/modernizr.custom.38010.js.download"></script>
+<!--[if lt IE 9]>
+    <script type="text/javascript" src="js/html5shiv.js"></script>
+    <script type="text/javascript" src="js/selectivizr-min.js"></script>
+    <script type="text/javascript" src="js/respond.min.js"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+	<script src="js/say-no-to-ie8.js"></script>
+<![endif]-->    
+    
+    <!-- FONTS -->
+    <link href="./index_files/css" rel="stylesheet" type="text/css">
+    <link href="./index_files/css(1)" rel="stylesheet" type="text/css">
+    <link href="./index_files/css(2)" rel="stylesheet" type="text/css">
+   
+    <!-- FONT AWESOME ICONS -->  
+    <link rel="stylesheet" href="./index_files/font-awesome.min.css">
+    
+    <!-- STYLES -->
+    <link rel="stylesheet" type="text/css" href="./index_files/normalize.css">
+    <link rel="stylesheet" type="text/css" href="./index_files/bookblock.css">
+    <link rel="stylesheet" type="text/css" href="./index_files/jquery.fancybox.css">
+    <link rel="stylesheet" type="text/css" href="./index_files/bjqs.css">
+    <link rel="stylesheet" type="text/css" href="./index_files/component.css">
+    <link rel="stylesheet" type="text/css" href="./index_files/style.css">
+    <link rel="stylesheet" type="text/css" href="./index_files/responsive.css">
+    <link rel="stylesheet" type="text/css" href="./index_files/perfect-scrollbar-0.4.8.min.css">
+    
+	<!-- skin STYLE -->
+    <link rel="stylesheet" type="text/css" href="./index_files/light-intro.css">
+    <link rel="stylesheet" type="text/css" href="./index_files/illustrator.css">
+    
+    <link rel="stylesheet" type="text/css" href="./index_files/custom.css">
+
+
+<style type="text/css">.fancybox-margin{margin-right:0px;}</style><script type="text/javascript" charset="UTF-8" src="./index_files/common.js.download"></script><script type="text/javascript" charset="UTF-8" src="./index_files/util.js.download"></script></head>
+
+<body style="">
+ 
+<div id="main-loading" style="display: none;"></div>
+    
+<div id="scroll-wrap" class="main book-open" style="height: 938px; display: none;">
+
+    <div class="book" data-book="book-1" style="top: 169px; margin-left: 50%;"><div class="cover"><a href="http://www.kevin-li.com/magicbook/index-illustrator.html###"><img src="./index_files/page-corner.png" id="page-corner"></a><div class="front"></div><div class="inner inner-left"></div></div><div class="inner inner-right"></div></div> 
+    
+    <div class="intro-wrapper" style="top: 369px; display: none;">
+          <div class="intro-content">
+              <h1>August Tree</h1>
+              <div id="aline"></div>
+              <p>The people who get on in this world are the people who get up and look for circumstances they want, and if they cannot find them, make them.</p>
+              <button id="open-it" class="btn">Open it</button>
+          </div>
+    </div>
+    
+</div><!-- /scroll-wrap -->        
+
+        
+<div id="top-perspective" class="effect-moveleft" style=""><!-- Fullscreen BookBlock -->
+	<div id="top-wrapper" style="left: 360px; top: 69px;">           
+		<div class="bb-custom-wrapper book-show" id="book-1">
+			<div class="bb-bookblock bb-vertical">
+
+                <!-- START ITEM -->
+                <div class="bb-item" style="display: block;"> <!--page-1~2:start-->
+                         
+                    <div class="bb-custom-side"> <!--page-1:start-->
+                        <div class="content-wrapper ps-container">
+                            <div class="container">
+
+                                <?php
+                                $fn = fopen("list_paras_en.txt","r");
+                                while(! feof($fn))  {
+                                $result = fgets($fn);
+                                echo "<p>$result</p>";
+                                }
+                                fclose($fn);
+                                ?>
+                            </div><!--container 1-->
+                        <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 3px; width: 100px; display: none;"><div class="ps-scrollbar-x" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; right: 3px; height: 100px; display: none;"><div class="ps-scrollbar-y" style="top: 0px; height: 0px;"></div></div></div><!--content-wrapper 1-->
+                    <div class="page-foot-cover"></div><div class="page-head-cover"></div><a href="http://www.kevin-li.com/magicbook/index-illustrator.html#" class="bb-flip bb-nav-prev" style="display: none;">Previous<img src="./index_files/nav-prev.png"></a></div><!--bb-custom-side page-1:end-->
+                    
+                    <div class="bb-custom-side">  <!--page-2:start-->
+                        <div class="content-wrapper ps-container">
+                            <div class="container">
+                                <?php
+                                $fn = fopen("list_paras_vi.txt","r");
+                                while(! feof($fn))  {
+                                $result = fgets($fn);
+                                echo "<p>$result</p>";
+                                }
+                                fclose($fn);
+                                ?>
+                            </div><!--container 2-->
+                        <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 3px; width: 100px; display: none;"><div class="ps-scrollbar-x" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; right: 3px; height: 100px; display: none;"><div class="ps-scrollbar-y" style="top: 0px; height: 0px;"></div></div></div><!--content-wrapper 2-->
+                    <a class="menu-button">"Show Menu"<div style="width: 30px;"></div><div style="width: 20px;"></div><div style="width: 10px;"></div></a><div class="page-head-cover"></div><div class="page-foot-cover"></div><a href="http://www.kevin-li.com/magicbook/index-illustrator.html#" class="bb-flip bb-nav-next">Next<img src="./index_files/nav-next.png"></a></div><!--bb-custom-side page-2:end-->
+                    
+                </div><!--bb-item-page 1~2:end-->
+                <!-- END ITEM -->
+
+                
+
+                
+			</div><!-- /bb-bookblock -->
+
+		</div><!-- /bb-custom-wrapper -->        
+	</div><!-- /top-wrapper  -->
+    
+    <div id="phone-menu"> <!-- Menu for phone scroll  -->
+      <a class="menu-button">"Show Menu"<div></div><div></div><div></div></a>
+    </div>
+    
+    <div id="menu-wrapper"><!-- Main menu  -->
+    	<span id="close-tip">back to cover</span>
+    	<a href="http://www.kevin-li.com/magicbook/index-illustrator.html#" id="close-button">×</a>
+        <nav class="outer-nav ps-container">
+        	<div id="nav-scroll">
+                <a href="http://www.kevin-li.com/magicbook/index-illustrator.html#" class="bb-current">Start</a>
+                <a href="http://www.kevin-li.com/magicbook/index-illustrator.html#" class="">About</a>
+                <a href="http://www.kevin-li.com/magicbook/index-illustrator.html#" class="">Portfolio</a>
+                <a href="http://www.kevin-li.com/magicbook/index-illustrator.html#" class="">Contact</a>
+            </div>
+        <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 3px; width: 360px; display: none;"><div class="ps-scrollbar-x" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; right: 3px; height: 570px; display: none;"><div class="ps-scrollbar-y" style="top: 0px; height: 0px;"></div></div></nav>
+        <span id="menu-copy">© kevin-li.com</span>
+    </div><!-- /menu-wrapper  -->
+    
+</div><!-- /top-perspective  -->
+
+<!-- Say no to IE 8 ...  -->
+
+<!--[if lt IE 9]>
+	<div id="say-no-to-ie8"><h3>Sorry, we no longer support your browser anymore :(</h3><h5>Please update it to later version, just like:</h5><span><a target="_blank" href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">Newer IE</a></span>|<span><a target="_blank" href="https://www.mozilla.org/en-US/firefox/new/#">Firefox</a></span>|<span><a target="_blank" href="http://www.opera.com/computer/windows">Opera</a></span>|<span><a target="_blank" href="https://www.google.com/intl/en/chrome/browser/">Chrome</a></span></div>
+<![endif]-->
+        
+	  <script type="text/javascript" src="./index_files/jquery-1.10.2.min.js.download"></script>        
+      <script type="text/javascript" src="./index_files/jquery.easing.min.js.download"></script>
+      <script src="./index_files/js" type="text/javascript"></script>
+      <script src="./index_files/jquery.bookblock.js.download"></script>
+      <script src="./index_files/jquery.placeholder.js.download"></script>
+	  <script src="./index_files/retina-1.1.0.min.js.download"></script>
+      <script src="./index_files/jquery.fancybox.pack.js.download"></script>
+	  <script src="./index_files/jquery.fancybox-media.js.download"></script>
+      <script src="./index_files/jquery.validate.min.js.download"></script>
+      <script src="./index_files/jquery.isotope.min.js.download"></script> 
+      <script src="./index_files/bjqs-1.3.js.download"></script> 
+      <script src="./index_files/gmap3.min.js.download"></script>  
+      <script src="./index_files/jquerypp.custom.js.download"></script>
+      <script src="./index_files/perfect-scrollbar-0.4.8.min.js.download"></script>
+      <script src="./index_files/jquery.mousewheel.js.download"></script>
+      <script src="./index_files/classie.js.download"></script>
+      <script src="./index_files/send-mail.js.download"></script>
+      <script src="./index_files/script.js.download"></script>
+      <script src="./index_files/menu.js.download"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-49961357-4', 'kevinstudio.net');
+	  ga('send', 'pageview');
+
+	</script>
+
+
+</body></html>
